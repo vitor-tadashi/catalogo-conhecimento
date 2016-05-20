978e9a1795bbd4c9fc2311a4b29791bd4873e9c5
Ryuki <ryukimag@gmail.com>
2016-05-20 17:31:15 -0300

    Adicionado método na Tecnologia Business e Dao de obterPorNome()

--- src/br/com/resource/catalogoconhecimento/dao/TecnologiaDAO.java
+++ src/br/com/resource/catalogoconhecimento/dao/TecnologiaDAO.java
@@ -55,6 +55,30 @@ public class TecnologiaDAO {
 		conexao.close();
 		return tecnologias;
 	}
+	
+	public TecnologiaBean obterPorNome(String nome) throws ClassNotFoundException, SQLException {
+		Connection conexao = ConnectionFactory.createConnection();
+
+		String sql = "SELECT * FROM Tecnologia WHERE nomeTecnologia = ?";
+		
+		PreparedStatement ps = conexao.prepareStatement(sql);
+		ps.setString(1, nome);
+		ResultSet rs = ps.executeQuery();
+
+		TecnologiaBean tecnologia = null;
+		
+		while (rs.next()) {
+			int idTecnologia = rs.getInt("idTecnologia");
+			String nomeTecnologia = rs.getString("nomeTecnologia");
+			
+			tecnologia = new TecnologiaBean();
+			tecnologia.setIdTecnologia(idTecnologia);
+			tecnologia.setNomeTecnologia(nomeTecnologia);
+		}
+
+		conexao.close();
+		return tecnologia;
+	}
 
 	// ATUALIZA
 	public void atualizar(TecnologiaBean tecnologia) throws ClassNotFoundException, SQLException {
