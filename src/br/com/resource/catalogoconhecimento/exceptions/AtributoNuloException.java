2688bb60a48e361d3d642dc73e32baff8eb6ca9d
Gustavo Alves <gustavo.santos@resource.com.br>
2016-05-24 11:41:00 -0300

    Classe de Exception para tratar parâmetros vazios

---
+++ src/br/com/resource/catalogoconhecimento/exceptions/AtributoNuloException.java
@@ -0,0 +1,35 @@
+package br.com.resource.catalogoconhecimento.exceptions;
+
+public class AtributoNuloException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AtributoNuloException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AtributoNuloException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AtributoNuloException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AtributoNuloException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AtributoNuloException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
