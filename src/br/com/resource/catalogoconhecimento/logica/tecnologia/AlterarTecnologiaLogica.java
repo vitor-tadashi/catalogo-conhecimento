afeda73b72ce66f25efc366a76a3e5659e963afd
Gustavo Alves <gustavo.santos@resource.com.br>
2016-05-25 09:39:01 -0300

    Validação completa de atualizar tecnologia, com painel de erro no formulário jsp indicando qual foi o erro cometido pelo usuario

--- src/br/com/resource/catalogoconhecimento/logica/tecnologia/AlterarTecnologiaLogica.java
+++ src/br/com/resource/catalogoconhecimento/logica/tecnologia/AlterarTecnologiaLogica.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import br.com.resource.catalogoconhecimento.bean.TecnologiaBean;
 import br.com.resource.catalogoconhecimento.business.TecnologiaBusiness;
+import br.com.resource.catalogoconhecimento.exceptions.AtributoNuloException;
 import br.com.resource.catalogoconhecimento.logica.Logica;
 
 public class AlterarTecnologiaLogica implements Logica {
@@ -13,13 +14,18 @@ public class AlterarTecnologiaLogica implements Logica {
 	public String executar(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		int id = Integer.parseInt(request.getParameter("id"));
 		String nome = request.getParameter("nome");
+
+		if(nome.trim().equals("")){
+			throw new AtributoNuloException("Por favor, digite um nome válido!");
+		} else {
+		
 		TecnologiaBean tecnologia = new TecnologiaBean();
 		tecnologia.setId(id);
 		tecnologia.setNome(nome);
 
 		TecnologiaBusiness tecnologiaBusiness = new TecnologiaBusiness();
 		tecnologiaBusiness.alterar(tecnologia);
-
+		}
 		return "mvc?logica=tecnologia.ListarTecnologiaLogica";
 	}
 
