89c6b537bdac0cb30a7fa23b6e542cf4a264aac8
Cezar Vaz <cezar.vaz@resource.com.br>
2016-06-06 14:15:56 -0300

    Refatoração da classe de teste

--- test/br/com/resource/catalogoconhecimento/tecnologia/TecnologiaTeste.java
+++ test/br/com/resource/catalogoconhecimento/tecnologia/TecnologiaTeste.java
@@ -2,6 +2,7 @@ package br.com.resource.catalogoconhecimento.tecnologia;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -21,31 +22,44 @@ public class TecnologiaTeste {
 
 	@Test
 	public void adicionarTecnologiaComSucesso() {
-		String tecnologia = "Selenium" + t;
-		driver.get(
-				"http://localhost:8080/catalogoconhecimento/mvc?logica=tecnologia.FormularioAdicionarTecnologiaLogica");
-		driver.findElement(By.name("nome")).sendKeys(tecnologia);
-		driver.findElement(By.id("submit")).click();
+		String tecnologia = adicionarTecnologia();
 
 		Assert.assertTrue("Deveria conter: " + tecnologia, driver.getPageSource().contains(tecnologia));
 	}
 
-	@Test
-	public void alterarTecnologiaComSucesso() {
+	// @Test
+	// public void alterarTecnologiaComSucesso() {
+	// String tecnologia = adicionarTecnologia();
+	//
+	// driver.findElement(By.name("alt" + tecnologia)).sendKeys(tecnologia);
+	//
+	// long t = new Timestamp(System.currentTimeMillis()).getTime();
+	// tecnologia = "Selenium" + t;
+	// driver.findElement(By.name("nome")).sendKeys(tecnologia);
+	// driver.findElement(By.id("submit")).click();
+	//
+	// Assert.assertTrue("Deveria conter: " + tecnologia,
+	// driver.getPageSource().contains(tecnologia));
+	// }
+
+	// @Test
+	// public void removerTecnologiaComSucesso() {
+	// String tecnologia = adicionarTecnologia();
+	//
+	// driver.findElement(By.name("del" + tecnologia)).sendKeys(tecnologia);
+	//
+	// Assert.assertFalse("Não deveria conter: " + tecnologia,
+	// driver.getPageSource().contains(tecnologia));
+	// }
+
+	public String adicionarTecnologia() {
 		String tecnologia = "Selenium" + t;
 		driver.get(
 				"http://localhost:8080/catalogoconhecimento/mvc?logica=tecnologia.FormularioAdicionarTecnologiaLogica");
 		driver.findElement(By.name("nome")).sendKeys(tecnologia);
 		driver.findElement(By.id("submit")).click();
 
-		driver.findElement(By.name("alt" + tecnologia)).sendKeys(tecnologia);
-
-		long t = new Timestamp(System.currentTimeMillis()).getTime();
-		tecnologia = "Selenium" + t;
-		driver.findElement(By.name("nome")).sendKeys(tecnologia);
-		driver.findElement(By.id("submit")).click();
-
-		Assert.assertTrue("Deveria conter: " + tecnologia, driver.getPageSource().contains(tecnologia));
+		return tecnologia;
 	}
 
 	@Before
@@ -55,14 +69,17 @@ public class TecnologiaTeste {
 		cliArgsCap.add("--web-security=false");
 		cliArgsCap.add("--ssl-protocol=any");
 		cliArgsCap.add("--ignore-ssl-errors=true");
-		cliArgsCap.add("--webdriver-loglevel=INFO");
+		cliArgsCap.add("--webdriver-loglevel=DEBUG");
 		cliArgsCap.add("--load-images=false");
 
+		capabilities.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, true);
 		capabilities.setCapability(CapabilityType.SUPPORTS_FINDING_BY_CSS, true);
-		capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
+		capabilities.setCapability(CapabilityType.ELEMENT_SCROLL_BEHAVIOR, true);
+		capabilities.setCapability(CapabilityType.HAS_NATIVE_EVENTS, true);
+		capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, false);
 		capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
 		driver = new PhantomJSDriver(capabilities);
-		// driver = new PhantomJSDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@After
