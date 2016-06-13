3dce568f119f62f5a7ee7d06b27230a328dcbcd2
Gustavo Alves <gustavo.santos@resource.com.br>
2016-06-13 11:23:06 -0300

    Tratamento de exceptions na busca com spring mvc

--- src/br/com/resource/catalogoconhecimento/controller/BuscaController.java
+++ src/br/com/resource/catalogoconhecimento/controller/BuscaController.java
@@ -3,10 +3,14 @@ package br.com.resource.catalogoconhecimento.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import br.com.resource.catalogoconhecimento.bean.FuncionarioBean;
 import br.com.resource.catalogoconhecimento.bean.ProjetoBean;
 import br.com.resource.catalogoconhecimento.business.FuncionarioBusiness;
@@ -14,6 +18,7 @@ import br.com.resource.catalogoconhecimento.business.NegocioBusiness;
 import br.com.resource.catalogoconhecimento.business.ProjetoBusiness;
 import br.com.resource.catalogoconhecimento.business.TecnologiaBusiness;
 import br.com.resource.catalogoconhecimento.exceptions.BusinessException;
+import br.com.resource.catalogoconhecimento.utils.ExceptionUtil;
 
 @Controller
 public class BuscaController {
@@ -32,6 +37,10 @@ public class BuscaController {
 
 	@RequestMapping(value = "listarPorNegocio", method = RequestMethod.POST)
 	public String listarPorNegocio(String filtro, Model model) throws BusinessException {
+
+		if (filtro.trim().equals("")) {
+			throw new BusinessException("Por favor, preencha o campo de busca!");
+		}
 
 		String[] array = filtro.split(",");
 		String nomeNegocio = "";
@@ -61,6 +70,11 @@ public class BuscaController {
 
 	@RequestMapping(value = "listarPorTecnologia", method = RequestMethod.POST)
 	public String listarPorTecnologias(String filtro, Model model) throws BusinessException {
+
+		if (filtro.trim().equals("")) {
+			throw new BusinessException("Por favor, preencha o campo de busca!");
+		}
+
 		String[] array = filtro.split(",");
 
 		String nomeTecnologias = "";
@@ -88,6 +102,13 @@ public class BuscaController {
 		model.addAttribute("funcionarios", listaFuncionario);
 
 		return "busca/listarBuscaTecnologia";
+	}
+
+	@ExceptionHandler(BusinessException.class)
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	public String exceptionHandler(Model model, BusinessException exception) {
+		model.addAttribute("msgErro", exception.getMessage());
+		return "index";
 	}
 
 }
--- src/br/com/resource/catalogoconhecimento/utils/ExceptionUtil.java
+++ src/br/com/resource/catalogoconhecimento/utils/ExceptionUtil.java
@@ -2,8 +2,16 @@ package br.com.resource.catalogoconhecimento.utils;
 
 import br.com.resource.catalogoconhecimento.exceptions.AtributoNuloException;
 import br.com.resource.catalogoconhecimento.exceptions.BusinessException;
+import br.com.resource.catalogoconhecimento.exceptions.ConsultaNulaException;
+import br.com.resource.catalogoconhecimento.exceptions.CpfInvalidoException;
+import br.com.resource.catalogoconhecimento.exceptions.DataInvalidaException;
+import br.com.resource.catalogoconhecimento.exceptions.EmailInvalidoException;
 import br.com.resource.catalogoconhecimento.exceptions.NomeRepetidoException;
+import br.com.resource.catalogoconhecimento.exceptions.QuantidadeTagException;
+import br.com.resource.catalogoconhecimento.exceptions.RegistroVinculadoException;
+import br.com.resource.catalogoconhecimento.exceptions.RgInvalidoException;
 import br.com.resource.catalogoconhecimento.exceptions.TamanhoCampoException;
+import br.com.resource.catalogoconhecimento.exceptions.UserInvalidoException;
 
 public abstract class ExceptionUtil {
 
@@ -16,6 +24,22 @@ public abstract class ExceptionUtil {
 			error = "Por favor, digite todos os campos";
 		}else if(e instanceof TamanhoCampoException){
 			error = "Você excedeu o numero de caracteres";
+		}else if(e instanceof ConsultaNulaException){
+			error = "Sua busca não retornou nenhum resultado";
+		}else if(e instanceof CpfInvalidoException){
+			error = "Por favor, digite um cpf válido";
+		}else if(e instanceof DataInvalidaException){
+			error = "Por favor, digite uma data válida";
+		}else if(e instanceof EmailInvalidoException){
+			error = "Por favor, digite um email válido";
+		}else if(e instanceof QuantidadeTagException){
+			error = "Por favor, preencha os campos de busca";
+		}else if(e instanceof RegistroVinculadoException){
+			error = "Não é possível excluir esse registro";
+		}else if(e instanceof RgInvalidoException){
+			error = "Por favor, digite um rg válido";
+		}else if(e instanceof UserInvalidoException){
+			error = "Nome de usuário inválido";
 		}
 		
 		return new BusinessException(error);
