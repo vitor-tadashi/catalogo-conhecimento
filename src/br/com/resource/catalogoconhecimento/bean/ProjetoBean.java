4c6f521d73ba9b5b775bc1d43729e1f8db6153c3
Lucas <Lucas>
2016-06-27 14:13:55 -0300

    Revert "."
    This reverts commit 122cb5c191acb548188edd4b0a5c9c54163b4805.

--- src/br/com/resource/catalogoconhecimento/bean/ProjetoBean.java
+++ src/br/com/resource/catalogoconhecimento/bean/ProjetoBean.java
@@ -1,23 +1,60 @@
 package br.com.resource.catalogoconhecimento.bean;
 
-import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Projeto")
 public class ProjetoBean {
-	
+
+	@Id
+	@GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+	@Column(name = "idProjeto", unique = true, nullable = false)
 	private int id;
+
+	@ManyToOne
+	@JoinColumn(name = "idCliente")
 	private ClienteBean cliente;
+
+	@Column(name = "nomeProjeto", nullable = false)
 	private String nome;
+
+	@Column(name = "observacao", nullable = false)
 	private String observacao;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "ProjetoNegocio", joinColumns = {
+			@JoinColumn(name = "idProjeto", nullable = false, updatable = false) }, inverseJoinColumns = {
+					@JoinColumn(name = "idNegocio", nullable = false, updatable = false) })
 	private List<NegocioBean> listaNegocio;
-	private List<TecnologiaBean>listaTecnologia;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "ProjetoTecnologia", joinColumns = {
+			@JoinColumn(name = "idProjeto", nullable = false, updatable = false) }, inverseJoinColumns = {
+					@JoinColumn(name = "idTecnologia", nullable = false, updatable = false) })
+	private List<TecnologiaBean> listaTecnologia;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "ProjetoEquipe", joinColumns = {
+			@JoinColumn(name = "idProjeto", nullable = false, updatable = false) }, inverseJoinColumns = {
+					@JoinColumn(name = "idEquipe", nullable = false, updatable = false) })
 	private List<EquipeBean> listaEquipe;
 
-	public ProjetoBean() {
-		listaNegocio = new ArrayList<>();
-		listaTecnologia = new ArrayList<>();
-		listaEquipe = new ArrayList<>();
-	}
+	// public ProjetoBean() {
+	// listaNegocio = new ArrayList<>();
+	// listaTecnologia = new ArrayList<>();
+	// listaEquipe = new ArrayList<>();
+	// }
 
 	public int getId() {
 		return id;
@@ -74,4 +111,5 @@ public class ProjetoBean {
 	public void setListaEquipe(List<EquipeBean> listaEquipe) {
 		this.listaEquipe = listaEquipe;
 	}
+
 }
