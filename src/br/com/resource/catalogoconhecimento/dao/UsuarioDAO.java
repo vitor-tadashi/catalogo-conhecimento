03075f0cccc9dbebaa9d1453f6d279f70c1d08f4
Cezar Vaz <cezar.vaz@resource.com.br>
2016-06-29 16:25:09 -0300

    Adicionada a anotação @Repository na classe UsuarioDAO

--- src/br/com/resource/catalogoconhecimento/dao/UsuarioDAO.java
+++ src/br/com/resource/catalogoconhecimento/dao/UsuarioDAO.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import javax.persistence.TypedQuery;
 
+import org.springframework.stereotype.Repository;
+
 import br.com.resource.catalogoconhecimento.bean.UsuarioBean;
 
+@Repository
 public class UsuarioDAO extends GenericDAOImpl<UsuarioBean, Integer> {
 
 	public List<UsuarioBean> obterPorNome(String nome) throws SQLException, ClassNotFoundException {
