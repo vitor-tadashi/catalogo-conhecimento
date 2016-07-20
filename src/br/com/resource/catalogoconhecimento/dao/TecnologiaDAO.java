90c85d0ed31d17df09e6a9f90f426abadbc9c56c
RodrigoRiviera <rodrigoriviera121@gmail.com>
2016-07-20 13:32:03 -0300

    feito a jpql de tecnologia ( listar por funcionários)

--- src/br/com/resource/catalogoconhecimento/dao/TecnologiaDAO.java
+++ src/br/com/resource/catalogoconhecimento/dao/TecnologiaDAO.java
@@ -6,11 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.TypedQuery;
-
 import org.springframework.stereotype.Repository;
-
 import br.com.resource.catalogoconhecimento.bean.ProjetoBean;
 import br.com.resource.catalogoconhecimento.bean.TecnologiaBean;
 import br.com.resource.catalogoconhecimento.factory.ConnectionFactory;
@@ -41,33 +38,14 @@ public class TecnologiaDAO extends GenericDAOImpl<TecnologiaBean, Integer> {
 	 * @throws SQLException
 	 */
 	public List<TecnologiaBean> listarPorFuncionario(int idFuncionario) throws ClassNotFoundException, SQLException {
-		Connection conexao = ConnectionFactory.createConnection();
 		
-		String sql = "SELECT t.idTecnologia, t.nomeTecnologia FROM Tecnologia AS t "
-				+ "INNER JOIN TecnologiaFuncionario AS tf ON tf.idTecnologia = t.idTecnologia "
-				+ "INNER JOIN Funcionario AS f on tf.idFuncionario = f.idFuncionario "
-				+ "WHERE f.idFuncionario = ? and t.ativo=?";
+		TypedQuery<TecnologiaBean> query = entityManager.createQuery("SELECT t FROM FuncionarioBean as f JOIN f.listaTecnologia AS t WHERE f.id = :id AND f.ativo = 'S'  ",TecnologiaBean.class);
+		query.setParameter("id", idFuncionario);
+		return query.getResultList();
 		
-		PreparedStatement ps = conexao.prepareStatement(sql);
-		ps.setInt(1, idFuncionario);
-		ps.setString(2, "s");
-		
-		ResultSet rs = ps.executeQuery();
-
-		List<TecnologiaBean> listaTecnologia = new ArrayList<TecnologiaBean>();
-		while (rs.next()) {
-			TecnologiaBean tecnologiaBean = new TecnologiaBean();
-			tecnologiaBean.setId(rs.getInt("idTecnologia"));
-			tecnologiaBean.setNome(rs.getString("nomeTecnologia"));
-
-			listaTecnologia.add(tecnologiaBean);
-		}
-		
-		ps.close();
-		conexao.close();
-
-		return listaTecnologia;
 	}
+	
+	
 	
 	/**
 	 * Lista todas as tecnologias de um projeto
